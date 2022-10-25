d65d66263ee8225aa2c3fc04a5f581768df8b855
The-Number1-Best-Coder <98104846+The-Number1-Best-Coder@users.noreply.github.com>
2022-10-25 19:15:07 +0530

    Added functionality to the code
    Changes to void loop sections: Introduced new variables ; Added necessary code for functionality ; The led is on when the distance value is less than 25.

--- LightDistance.ino
+++ LightDistance.ino
@@ -1,13 +1,38 @@
 const int echo = 4;
 const int trig = 5;
 const int led = 5;
+float distance,duration;
 
 void setup(){
  Serial.begin(9600);
  pinMode(echo, INPUT);
  pinMode(trig, OUTPUT);
  pinMode(led, OUTPUT);
+ Serial.begin(9600);
 }
-void loop(){
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(trig , LOW);
+delayMicroseconds(2);
+digitalWrite(trig , HIGH);
+delayMicroseconds(10);
+digitalWrite(trig , LOW);
+noInterrupts();
+duration = pulseIn(echo , HIGH);
+interrupts();
+
+distance = (duration/2) * 0.034;
+//(duration/2) * 0.034;
+Serial.print(distance );
+Serial.println(" cm");
  
+if(distance<25){
+digitalWrite(led , HIGH);
+;
+}
+
+if(distance>25){
+digitalWrite(led , LOW);
+
+}
 }
