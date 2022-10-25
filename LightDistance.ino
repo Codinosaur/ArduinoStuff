ee24d2a3fd79c3914e218b2b24e0f2b0bd8140d0
AnYellowKing <95577537+AnYellowKing@users.noreply.github.com>
2022-10-25 11:20:26 +0530

    Create LightDistance.ino

---
+++ LightDistance.ino
@@ -0,0 +1,12 @@
+const int echo = 4;
+const int trig = 5;
+const int led = ;
+
+void setup(){
+ Serial.begin(9600);
+ pinMode(echo, INPUT);
+ pinMode(trig, OUTPUT);
+ pinMode(led, OUTPUT);
+
+void loop(){
+ 
